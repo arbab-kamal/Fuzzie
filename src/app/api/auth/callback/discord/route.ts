75ff7b1a9f2faad71861f12477452eb59e91b8d5
wingie125 <arbab.vns123@gmail.com>
2024-11-28 21:08:59 +0530

    trial

--- src/app/api/auth/callback/discord/route.ts
+++ src/app/api/auth/callback/discord/route.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 import { NextResponse, NextRequest } from "next/server";
 import url from "url";
 
-const Public_url = "https://fuzzie-seven.vercel.app";
-
 export async function GET(req: NextRequest) {
   const code = req.nextUrl.searchParams.get("code");
   if (code) {
@@ -11,7 +9,10 @@ export async function GET(req: NextRequest) {
     data.append("client_id", process.env.DISCORD_CLIENT_ID!);
     data.append("client_secret", process.env.DISCORD_CLIENT_SECRET!);
     data.append("grant_type", "authorization_code");
-    data.append("redirect_uri", `${Public_url}/api/auth/callback/discord`);
+    data.append(
+      "redirect_uri",
+      `${process.env.NEXT_PUBLIC_URL}/api/auth/callback/discord`
+    );
     data.append("code", code.toString());
 
     const output = await axios.post(
@@ -40,10 +41,10 @@ export async function GET(req: NextRequest) {
       );
 
       return NextResponse.redirect(
-        `${Public_url}/connections?webhook_id=${output.data.webhook.id}&webhook_url=${output.data.webhook.url}&webhook_name=${output.data.webhook.name}&guild_id=${output.data.webhook.guild_id}&guild_name=${UserGuild[0].name}&channel_id=${output.data.webhook.channel_id}`
+        `${process.env.NEXT_PUBLIC_URL}/connections?webhook_id=${output.data.webhook.id}&webhook_url=${output.data.webhook.url}&webhook_name=${output.data.webhook.name}&guild_id=${output.data.webhook.guild_id}&guild_name=${UserGuild[0].name}&channel_id=${output.data.webhook.channel_id}`
       );
     }
 
-    return NextResponse.redirect(`${Public_url}/connections`);
+    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/connections`);
   }
 }
